649cd4c3d02fb44daa7e027e43222fa245bcc3ee
Matheus <matheusmledra@gmail.com>
2020-08-04 17:44:31 -0300

    Aprendido a como fazer um code com double e utilizando no println

---
+++ UsandoCasaDecimal.java
@@ -0,0 +1,14 @@
+package Curso;
+
+public class UsandoCasaDecimal{
+
+	public static void main (String[] args) {
+		String nome;
+		double altura;
+		
+		nome = "Goku ";
+		altura = 1.83;
+		
+		System.out.println("Oi eu sou o " + nome + "! E tenho " + altura + " metros");
+	}
+}
