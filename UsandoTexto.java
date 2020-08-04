2e719cd8bad9e6f936960904a66be3e6a8b13fcd
Matheus <matheusmledra@gmail.com>
2020-08-04 17:42:33 -0300

    Aprendido a como fazer um code usando uma String no println

---
+++ UsandoTexto.java
@@ -0,0 +1,12 @@
+package Curso;
+
+public class UsandoTexto{
+
+	public static void main (String[] args) {
+		String nome;
+		
+		nome = "Matheus Momm Ledra";
+		
+		System.out.println("Meu nome é " + nome);
+	}
+}
