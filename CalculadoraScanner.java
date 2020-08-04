0c70b9d4ffdd4ae1f3bdbcf914dd38026b17a173
Matheus <matheusmledra@gmail.com>
2020-08-04 17:44:59 -0300

    Exercício de fazer uma calculadora usando o Scanner

---
+++ CalculadoraScanner.java
@@ -0,0 +1,71 @@
+package Curso;
+
+import java.util.Scanner;
+
+public class CalculadoraScanner {
+
+	public static void main(String[] args) { 
+		
+		System.out.println("==================");
+		System.out.println("Escolha uma opção:");
+		System.out.println("Adição = 1");
+		System.out.println("Subtração = 2");
+		System.out.println("Multiplicação = 3");
+		System.out.println("Divisão = 4");
+		System.out.println("==================");
+		
+		Scanner ope = new Scanner(System.in);
+		int operacao = ope.nextInt();
+		
+		if(operacao == (1)) {
+			Scanner num = new Scanner(System.in);
+			
+			System.out.println("Primeiro número:");
+			int num1 = num.nextInt();
+			System.out.println("Segundo número:");
+			int num2 = num.nextInt();
+			
+			int resultado = num1 + num2;
+			
+			System.out.println("O resultado é: " + resultado);
+		}
+		if(operacao == (2)) {
+			Scanner num = new Scanner(System.in);
+			
+			System.out.println("Primeiro número:");
+			int num1 = num.nextInt();
+			System.out.println("Segundo número:");
+			int num2 = num.nextInt();
+			
+			int resultado = num1 - num2;
+			
+			System.out.println("O resultado é: " + resultado);
+		}
+		if(operacao == (3)) {
+			Scanner num = new Scanner(System.in);
+			
+			System.out.println("Primeiro número:");
+			int num1 = num.nextInt();
+			System.out.println("Segundo número:");
+			int num2 = num.nextInt();
+			
+			int resultado = num1 * num2;
+			
+			System.out.println("O resultado é: " + resultado);
+		}
+		if(operacao == (4)) {
+			Scanner num = new Scanner(System.in);
+			
+			System.out.println("Primeiro número:");
+			int num1 = num.nextInt();
+			System.out.println("Segundo número:");
+			int num2 = num.nextInt();
+			
+			int resultado = num1 / num2;
+			
+			System.out.println("O resultado é: " + resultado);
+		}
+		
+	}
+	
+}
