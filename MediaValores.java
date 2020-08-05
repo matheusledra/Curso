2b7e0926a77fdeccb26f838f337f5c40a1cb34f8
Matheus Momm Ledra <68034073+Math1307@users.noreply.github.com>
2020-08-05 16:43:07 -0300

    Adicionado um exemplo para a média

--- MediaValores.java
+++ MediaValores.java
@@ -15,7 +15,7 @@ public class MediaValores {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Insira seus valores para média:");
+		System.out.println("Insira seus valores para mÃ©dia (Exemplo: 15 6 4 19)");
 		n1 = sc.nextDouble();
 		n2 = sc.nextDouble();
 		n3 = sc.nextDouble();
@@ -24,7 +24,7 @@ public class MediaValores {
 		soma = n1 + n2 + n3 + n4;
 		media = soma / 4;
 		
-		System.out.println("A média dos valores inseridos é: " + media);
+		System.out.println("A mÃ©dia dos valores inseridos Ã©: " + media);
 		
 		sc.close();
 	}
