1d7e424c1d6acadb18a62b9d12dac815bbd8542d
Matheus <matheusmledra@gmail.com>
2020-08-06 21:33:06 -0300

    Arrumado o code após o pull

--- MediaValores.java
+++ MediaValores.java
@@ -15,11 +15,8 @@ public class MediaValores {
 		
 		Scanner sc = new Scanner(System.in);
 		
-<<<<<<< HEAD
 		System.out.println("Insira seus valores para média (Exemplo: 15 6 4 19)");
-=======
-		System.out.println("Insira seus valores para mÃ©dia (Exemplo: 15 6 4 19)");
->>>>>>> 2b7e0926a77fdeccb26f838f337f5c40a1cb34f8
+		
 		n1 = sc.nextDouble();
 		n2 = sc.nextDouble();
 		n3 = sc.nextDouble();
@@ -28,7 +25,7 @@ public class MediaValores {
 		soma = n1 + n2 + n3 + n4;
 		media = soma / 4;
 		
-		System.out.println("A mÃ©dia dos valores inseridos Ã©: " + media);
+		System.out.println("A média dos valores inseridos é: " + media);
 		
 		sc.close();
 	}
