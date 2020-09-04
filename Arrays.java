c34943ade7f123e2fd404a7bc2ba9aa3705cd32b
Matheus <matheusmledra@gmail.com>
2020-09-04 12:19:53 -0300

    Aprendido Arrays

---
+++ Arrays.java
@@ -0,0 +1,15 @@
+package Curso;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int[] arrayNumeros = {10, 20, 30, 40, 50};
+		String[] arrayString = {"Batata", "Arroz"};
+		
+		System.out.println("Array Números:");
+		System.out.println("Na posição 1 (indice 0) -> " + arrayNumeros[0]);
+		System.out.println("");
+		System.out.println("Array Strings:");
+		System.out.println(arrayString[1]);
+	}
+}
