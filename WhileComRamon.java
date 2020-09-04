3f410602f605f84f6b8b118e7115743c658276fd
Matheus <matheusmledra@gmail.com>
2020-09-04 12:18:10 -0300

    Aprendido While com o professor Ramon

---
+++ WhileComRamon.java
@@ -0,0 +1,16 @@
+package Curso;
+
+public class WhileComRamon {
+
+	public static void main(String[] args) {
+		
+		int numeros = 1, vezes = 0;
+		int resultado;
+		
+		while(numeros <= 100) {
+			System.out.println("");
+			numeros ++;
+		}
+		System.out.println("Console limpo");
+	}
+}
