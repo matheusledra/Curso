90630c26bd6d0257e17e8f0cd66f4d7476d0f7a5
Matheus <matheusmledra@gmail.com>
2020-10-27 16:30:19 -0300

    Projeto de compromissos refeito (O primeiro não foi enviado e eu perdi)

---
+++ NetBeans/ProjetoCompromissosRefazendo/src/br/com/beans/Compromisso.java
@@ -0,0 +1,59 @@
+package br.com.beans;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Compromisso {
+
+    private String local, titulo, status;
+    private Calendar data = Calendar.getInstance();
+
+    public Compromisso() {
+    }
+
+    public Compromisso(String local, String titulo, String status, String data) throws ParseException {
+        this.local = local;
+        this.titulo = titulo;
+        this.status = status;
+        this.setData(data);
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getData() {
+        SimpleDateFormat dataFormat = new SimpleDateFormat("dd/MM/yyyy");
+        return dataFormat.format(this.data.getTime());
+    }
+
+    public void setData(String data) throws ParseException {
+        SimpleDateFormat dataFormat = new SimpleDateFormat("dd/MM/yyyy");
+        this.data.setTime(dataFormat.parse(data));
+    }
+
+    
+    
+}
---
+++ NetBeans/ProjetoCompromissosRefazendo/src/br/com/beans/Usuario.java
@@ -0,0 +1,49 @@
+package br.com.beans;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+
+public class Usuario extends Pessoa{
+    
+    private String login, senha;
+    private ArrayList<Compromisso> compromisso = new ArrayList<Compromisso>();
+    
+    public Usuario(){}
+
+    public Usuario(String nome, int idade, String sexo, String login, String senha) {
+        setNome(nome);
+        setIdade(idade);
+        setSexo(sexo);
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    public void addCompromisso(String local, String titulo, String status, String data) throws ParseException {
+        Compromisso comp = new Compromisso(local, titulo, status, data);
+        this.compromisso.add(comp);
+    }
+    
+    public Compromisso getCompromisso(int indiceCompromisso) {
+        return this.compromisso.get(indiceCompromisso);
+    }
+    
+    public int getQntdCompromissos() {
+        return this.compromisso.size();
+    }
+}
